094ba9bb4fd61a23c38de0dd7f98cafdd21e5d97
Carlos Pineda <cara_p@unicah.edu>
2025-03-25 22:59:24 -0600

    update pom.xml and add HelloWorldController servlet for JSON response

---
+++ src/main/java/com/institute/centralreach/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.institute.centralreach;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloWorldController extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("{\"message\": \"Hello, World!\"}");
+        response.getWriter().flush();
+    }
+
+}
